dc104f63fbea8e12527556faa10738edfc738067
Olyadtemesgen <olexyit@gmail.com>
2022-06-02 12:38:28 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *main - This function prints out the following string to standard error
+ *and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *Return: This function returns 1
  */
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-		return (1);
+fprintf(stderr, "and that piece if art is useful\" - Dora Korpar, ");
+fprintf(stderr, "2015-10-19\n";
+return (1);
 }

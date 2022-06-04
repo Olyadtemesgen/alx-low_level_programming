b7b1ac9d5db562b918cb6fd75bffd919a09bbca2
Olyadtemesgen <olexyit@gmail.com>
2022-06-03 19:24:30 -0700

    add it

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this functions output is lowercase alphabet
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+for (char low_alpha = 'a' ; low_alpha <= 'z' ; low_alpha++)
+putchar(low_alpha);
+}
+putchar('\n');
+return (0);
+}

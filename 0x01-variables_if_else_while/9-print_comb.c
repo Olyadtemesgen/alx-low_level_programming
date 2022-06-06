4237fb740af7e3cf8c5759785d1f2419f95db356
Olyadtemesgen <olexyit@gmail.com>
2022-06-05 18:29:45 -0700

    0 upt0 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this function is used to return number separateed by commas
+ *
+ * Return:always 0 (Success)
+ */
+int main(void)
+{
+int a = 47;
+while (a < 57)
+{
+putchar(a);
+putchar(44);
+putchar(32);
+}
+putchar(57);
+return (0);
+}
+

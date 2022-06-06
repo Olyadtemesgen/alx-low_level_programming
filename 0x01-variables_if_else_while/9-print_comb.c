a01b765004eb25f3687eaddc54d7b6305ede7b88
Olyadtemesgen <olexyit@gmail.com>
2022-06-05 20:13:38 -0700

    aa

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,21 @@
-#include <stdio.h>
+include <stdio.h>
 /**
- * main-this function is used to return number separateed by commas
- *
- *Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-int a = 48;
-
-while (a < 58)
-{
-if (a < 57)
-{
-putchar(a);
-putchar(44);
+	int a;
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (a < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-else if (a == 57)
-{
-putchar(57);
-}
-putchar(32);
-a++;
-}
-return (0);
-}
-

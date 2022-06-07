5072a4a870674d18230c84ddd93af4b2dcd240e9
Olyadtemesgen <olexyit@gmail.com>
2022-06-07 16:41:51 -0700

    add

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - add nums
+ *
+ * Return: always 0
+ */
+void main(void)
+{
+	int i;
+	int j;
+
+	j = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i
+		}
+	}
+		printf("%d", j);
+	}
+

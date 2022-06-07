fbdc8e0e81933fb65d1585bf9c2e3e51822ad799
Olyadtemesgen <olexyit@gmail.com>
2022-06-07 10:35:59 -0700

    addd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * main - check the code
  *
@@ -5,12 +6,13 @@
  */
 void print_alphabet(void)
 {
-	int i =97;
+	int i = 97;
+
 	while (i < 123)
 	{
 		_putchar(i);
 		i++;
-	} 
+	}
 	_putchar('\n');
 
 }

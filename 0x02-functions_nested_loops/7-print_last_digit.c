b1cb77d177e7d9a97a8ee5161df1d9ad983636d7
Olyadtemesgen <olexyit@gmail.com>
2022-06-07 12:50:40 -0700

    add:

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - returns absolute value of an int
+ * @a: integer
+ * Return: Void.
+ */
+int print_last_digit(int a)
+{
+	int b = a % 10;
+
+	return (b);
+}
+

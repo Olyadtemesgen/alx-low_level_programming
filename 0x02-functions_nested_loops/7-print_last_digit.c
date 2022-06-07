bcaab5b9527bb5b20c13eb222a1a1b2df5321bcd
Olyadtemesgen <olexyit@gmail.com>
2022-06-07 13:02:04 -0700

    add

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,12 @@
  * @a: integer
  * Return: Void.
  */
-int print_last_digit(int a)
+void print_last_digit(int a)
 {
 	int b = a % 10;
 
 	_putchar(b + 48);
 	_putchar(b + 48);
 	_putchar('\n');
-	return (0);
 }
 

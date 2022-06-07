abba5047029fc57f7814ff3e16ebba41aa4ea2b6
Olyadtemesgen <olexyit@gmail.com>
2022-06-07 10:42:47 -0700

    add

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - check the code for Holberton School students
  *
- * Return: Always 0.
+ * Return: Always 0(Success)
  */
 void print_alphabet(void)
 {

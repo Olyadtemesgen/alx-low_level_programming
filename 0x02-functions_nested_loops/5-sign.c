ef47b1d817b995fa98b8304e37a3702c2ad0e06f
Olyadtemesgen <olexyit@gmail.com>
2022-06-07 12:16:13 -0700

    add

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+nclude "main.h"
+/**
+ * print_sign - print wether number is neg or pos
+ *@n: - it is a kind integer
+ *
+ * Return: -always zero for success
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}

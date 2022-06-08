6ac4763611fc798e21cf357dfecbe5308e699fc1
Olyadtemesgen <olexyit@gmail.com>
2022-06-08 11:39:54 -0700

    add

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_times_table - it print numbers
+ *@n: number between 0 and 15
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n < 16 || n >= 0)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				if (j < n)
+				{
+					if (i * j < 10 && (j + 1) * i < 10)
+					{
+						printf("%d,   ", i * j);
+					}
+					else if (i * j < 100 && (j + 1) * i > 9 && (j + 1) * i < 100)
+					{
+						printf("%d,  ", i * j);
+					}
+					else if (i * j < 100 && (j + 1) * i > 99)
+					{
+						printf("%d, ", i * j);
+					}
+					else
+					{
+						printf("%d", i * j);
+					}
+				}
+				else
+				{
+					printf("%d\n", n);
+				}
+			}
+			printf("\n");
+		}
+	}
+}

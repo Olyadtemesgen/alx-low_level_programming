37c9fd381b4fbd816f15ed68ce3df0cbc709f38a
Olyadtemesgen <olexyit@gmail.com>
2022-06-08 13:17:03 -0700

    add

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,10 +7,12 @@ int main(void)
 {
 unsigned long int a = 1;
 unsigned long int b = 1;
-for (int i = 1; i <= 92 ; i++)
+unsigned long int c;
+int i;
+for (i = 1; i <= 92 ; i++)
 {
 printf("%lu, ", b);
-unsigned long int c = a + b;
+c = a + b;
 a = b;
 b = c;
 }

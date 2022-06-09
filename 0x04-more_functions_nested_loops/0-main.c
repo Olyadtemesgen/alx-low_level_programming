9f30bbba1f23c6cbf65e9490ee4e4c5e33f0000e
Olyadtemesgen <olexyit@gmail.com>
2022-06-09 12:58:09 -0700

    add

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,17 +1,26 @@
 #include "main.h"
 /**
- * _isupper - tells whether  up
- *@c: ASCII num
+ * main - add numbers
+ * _isupper - add numberrrrr
  *Return: 0 or 1
  */
+int main(void)
+{
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+}
 int _isupper(int c)
 {
-	if (c < 91 && c > 64)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c < 91 && c > 64)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }

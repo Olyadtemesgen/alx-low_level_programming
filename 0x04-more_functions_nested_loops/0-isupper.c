40396e9a8c1811a08d718cf2ba2c3c80e339a790
Olyadtemesgen <olexyit@gmail.com>
2022-06-09 18:25:24 -0700

    sadda

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,15 @@
 #include "main.h"
 /**
- * main - add numbers
- * _isupper - add numberrrrr
- *Return: 0 or 1
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
  */
-int main(void)
-{
-char c;
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
-}
 int _isupper(int c)
 {
-if (c < 91 && c > 64)
+if (c >= 'A' && c <= 'Z')
 {
-return (1);
+return (1)
 }
 else
 {

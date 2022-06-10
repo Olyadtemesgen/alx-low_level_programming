edcf60dd07513442cf853e192d6c0b36dbf59ca4
Olyadtemesgen <olexyit@gmail.com>
2022-06-10 02:47:46 -0700

    add

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,18 @@
  */
 void print_line(int n)
 {
-	int i;
-	
-	if (n > 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
-	_putchar('\n');
-	}
-	else{
-		_putchar('\n');
-	}
+int i;
+
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
 }

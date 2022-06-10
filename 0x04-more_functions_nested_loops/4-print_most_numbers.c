2a1d48fe4f88a82b7c4bf033b01c67fbf1f13d43
Olyadtemesgen <olexyit@gmail.com>
2022-06-09 23:58:07 -0700

    add

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_most_numbers - print nums e 4 and 2
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	if (i != '2' || i != '4')
+	{
+		_putchar(i);
+	}
+	}
+}

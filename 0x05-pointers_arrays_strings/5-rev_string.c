46c0e66dd7d3ef8cce735b410ffe7fc2881ad7ad
Olyadtemesgen <olexyit@gmail.com>
2022-06-14 06:58:08 -0700

    addd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - it reverse string
+ * @s:it is anstring
+ * Return:void
+ */
+void rev_string(char *s)
+{
+	int a;
+
+	for (a = 0; s[a]; a++)
+	{
+		continue;
+	}
+	while (a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
+
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,5 @@ void _puts(char *str);
 void print_rev(char *s);
 void print_array(int *a, int n);
 void print_rev(char *s);
+void rev_string(char *s);
+

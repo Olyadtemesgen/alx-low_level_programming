4ad0003112b37c6db26e9281b15df978d778d4a5
Olyadtemesgen <olexyit@gmail.com>
2022-06-20 18:43:04 -0700

    add

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,3 +1,9 @@
+/**
+ * set_string -the function that return a character of a pointer
+ * @s: pointer of a pointer
+ * @to:pointer
+ * Return: void
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;

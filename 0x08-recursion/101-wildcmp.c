c345633f8bfe80453b705e2e5f890718da50b874
Olyadtemesgen <olexyit@gmail.com>
2022-06-22 15:40:25 -0700

    add

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1, s2 + 1));
 	}
-	else if (*s1 == '\0' && ! (*s2 != '\0' && *s2 == '*'))
+	else if (*s1 == '\0' && !(*s2 != '\0' && *s2 == '*'))
 	{
 		return (*s2 == '\0');
 	}

2a2ceb922a47f4d37e6abae29466fcfa0e315c30
Olyadtemesgen <olexyit@gmail.com>
2022-06-25 13:23:48 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * isInteger - checks if s is an integer
- *   * @s: string to check
- *    * Return: 0 or 1
- *     */
-
+ * isInteger - checks if an integerr
+ * @s: character
+ * Return: 0 or 1
+ */
 int isInteger(const char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
-	{
-			if (s[i] < '0' || s[i] > '9')
-						return (1);
-				i++;
-	}
-	return (0);
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] < '0' || s[i] > '9')
+return (1);
+i++;
 }
-
+return (0);
+}
 /**
  * main -it adds positive numbers
  * @argc: variavle count

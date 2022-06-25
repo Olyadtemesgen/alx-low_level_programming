780f7b0a0fc58770705cdb3c7f9f8047c9efe535
Olyadtemesgen <olexyit@gmail.com>
2022-06-25 13:03:37 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - it print the name
+ * @argv: argument vector returning string
+ * @argc: argument count
+ * Return: integer
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

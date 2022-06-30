249fa4683d361f017fe9ad61b34a16b5d4724b0b
Olyadtemesgen <olexyit@gmail.com>
2022-06-30 14:33:27 -0700

    add this too

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,15 +6,13 @@
  * malloc_checked - it returns a pointer of malloc
  * @pointer: it is a pointer
  * @b:an argument
- *return:pointer 
+ *return:pointer
  */
 void *malloc_checked(unsigned int b)
 {
 void *pointer;
 pointer = malloc(b);
 if (pointer == NULL)
-{
 exit(98);
-}
 return (pointer);
 }

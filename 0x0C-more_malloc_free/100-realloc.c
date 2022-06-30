7a42ac509df67affe087cbc918f1750215485967
Olyadtemesgen <olexyit@gmail.com>
2022-06-30 16:04:12 -0700

    add

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	void *new_ptr;
+
+	i = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+			return ('\0');
+
+		return (new_ptr);
+	}
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return ('\0');
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (!new_ptr)
+		return ('\0');
+
+	while (i < old_size)
+	{
+		*((char *)new_ptr + i) = *((char *)ptr + i);
+		i++;
+	}
+
+	free(ptr);
+	return ((void *)new_ptr);
+}

81d48f1cca0ce56d310f4783900b6d8fdd0bf435
Olyadtemesgen <olexyit@gmail.com>
2022-06-30 15:40:58 -0700

    add this shit too

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: members number(count)
+ * @size: another argument or parameter
+ * Return: pointer to the allocated memory (SUCCESS)
+ * NULL if @nmemb or @size is 0 (FAILURE)
+ * NULL if insufficient memory was available (FAILURE)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *pointer;
+	int i;
+
+	i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	pointer = malloc(nmemb * size);
+
+	if (pointer == NULL)
+		return ('\0');
+
+	while (i < nmemb * size)
+	{
+		pointer[i] = 0;
+		i++;
+	}
+	return ((void *)pointer);
+}

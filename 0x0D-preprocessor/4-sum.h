abd4764b97345f8416bc2b193992e776133e20b4
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-07-05 22:22:47 +0300

    edited

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- *main - prints the name of file it was compiled from
- *
- *Return:(0) - success
- *
- */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#ifndef SUMM
+#define SUMM
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /*SUM*/

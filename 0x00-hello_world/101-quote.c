e8eaf1da89306a55bab58ab817b1e53f3189e0f9
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-08-13 13:53:23 +0300

    added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece if art is useful\" - Dora Korpar, ");
-fprintf(stderr, "2015-10-19\n";)
+printf("and that piece if art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
